cad2bdd377a64ba9b8efda6d82a96a5fab61e08f
Farhad Dehghani <farhad@shirazconsult.com>
2013-06-04 10:07:25 +0200

    Using the Options class instead of Quintet

--- stats-mnm-agent/src/main/java/com/nordija/statistic/mnm/agent/StatsDataProvider.java
+++ stats-mnm-agent/src/main/java/com/nordija/statistic/mnm/agent/StatsDataProvider.java
@@ -13,7 +13,6 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.WebApplicationException;
 import javax.ws.rs.core.MediaType;
 
-import org.javatuples.Quintet;
 import org.joda.time.DateTime;
 import org.joda.time.DateTimeConstants;
 import org.joda.time.format.DateTimeFormatter;
@@ -125,7 +124,7 @@ public class StatsDataProvider {
 			@PathParam("options") String options) {
 		logger.debug("Returning statistics view rows from {} to {} for {} with options {}.", new Object[]{from, to, type, options});
 		try {
-			return fetch(type, from, to, extractOptions(options));
+			return fetch(type, from, to, new Options(options));
 		} catch (Exception e) {
 			logger.error("Failed to retrieve records for date interval {} - {}. Reason {}", new Object[]{from, to, e.getMessage()});
 			throw new WebApplicationException(e, 500);
@@ -181,9 +180,8 @@ public class StatsDataProvider {
 			for(int i=0; i< diffInHours; i++){
 				DateTime nextFrom = fromFloor.plusHours(i);
 				DateTime nextTo = nextFrom.plusHours(1);
-				Quintet<String, String, String, Integer, String> opts = extractOptions(options);
 				logger.debug("Fetching from {} to {}.", nextFrom, nextTo);
-				NestedList<Object> fetch = fetch(type, nextFrom.getMillis(), nextTo.getMillis(), opts.setAt4("hourly"));
+				NestedList<Object> fetch = fetch(type, nextFrom.getMillis(), nextTo.getMillis(), new Options(options).setTimeunit(TimeUnit.hourly));
 				res.addElement(fetch);
 			}
 			return res;
@@ -195,8 +193,7 @@ public class StatsDataProvider {
 				DateTime nextFrom = fromFloor.plusDays(i);
 				DateTime nextTo = nextFrom.plusDays(1);
 				logger.debug("Fetching from {} to {}.", nextFrom, nextTo);
-				Quintet<String, String, String, Integer, String> opts = extractOptions(options);
-				NestedList<Object> fetch = fetch(type, nextFrom.getMillis(), nextTo.getMillis(), opts.setAt4("daily"));
+				NestedList<Object> fetch = fetch(type, nextFrom.getMillis(), nextTo.getMillis(), new Options(options).setTimeunit(TimeUnit.daily));
 				res.addElement(fetch);
 			}
 			return res;
@@ -208,27 +205,30 @@ public class StatsDataProvider {
 				DateTime nextFrom = fromFloor.plusWeeks(i);
 				DateTime nextTo = nextFrom.plusWeeks(1);
 				logger.debug("Fetching from {} to {}.", nextFrom, nextTo);
-				Quintet<String, String, String, Integer, String> opts = extractOptions(options);
-				NestedList<Object> fetch = fetch(type, nextFrom.getMillis(), nextTo.getMillis(), opts.setAt4("weekly"));
+				NestedList<Object> fetch = fetch(type, nextFrom.getMillis(), nextTo.getMillis(), new Options(options).setTimeunit(TimeUnit.weekly));
 				res.addElement(fetch);
 			}
 			return res;
 		}else{
 			// divide into months and create up to 12 response lists as there are months
-			int diffInMonths = toDate.monthOfYear().get() - fromDate.monthOfYear().get();
-			Quintet<String, String, String, Integer, String> opts = extractOptions(options);
+			int diffInMonths = 0;
+			if(toDate.year().get() == fromDate.year().get()){
+				diffInMonths = toDate.monthOfYear().get() - fromDate.monthOfYear().get();
+			}else{
+				diffInMonths = 12 - fromDate.monthOfYear().get() + toDate.monthOfYear().get();
+			}
 			DateTime fromFloor = fromDate.monthOfYear().roundFloorCopy();
 			if(diffInMonths == 0){
 				DateTime nextTo = fromFloor.plusMonths(1);
 				logger.debug("Fetching from {} to {}.", fromFloor, nextTo);
-				NestedList<Object> fetch = fetch(type, fromFloor.getMillis(), nextTo.getMillis(), opts.setAt4("monthly"));
+				NestedList<Object> fetch = fetch(type, fromFloor.getMillis(), nextTo.getMillis(), new Options(options).setTimeunit(TimeUnit.weekly));
 				res.addElement(fetch);
 			}else{
 				for(int i=0; i < diffInMonths; i++){					
 					DateTime nextFrom = fromFloor.plusMonths(i);
 					DateTime nextTo = nextFrom.plusMonths(1);
 					logger.debug("Fetching from {} to {}.", nextFrom, nextTo);
-					NestedList<Object> fetch = fetch(type, nextFrom.getMillis(), nextTo.getMillis(), opts.setAt4("monthly"));
+					NestedList<Object> fetch = fetch(type, nextFrom.getMillis(), nextTo.getMillis(), new Options(options).setTimeunit(TimeUnit.monthly));
 					res.addElement(fetch);
 				}
 			}
@@ -238,26 +238,26 @@ public class StatsDataProvider {
 		return null;
 	}
 		
-	private NestedList<Object> fetch(String type, long from, long to, Quintet<String, String, String, Integer, String> options) {
+	private NestedList<Object> fetch(String type, long from, long to, Options options) {
 		NestedList<Object> res = new NestedList<Object>();
 		StringBuilder q = new StringBuilder("select `type`, `name`, ").
-				append(options.getValue0() != null && options.getValue0().equals("title") ? "`title`" : "'' as `title`").
+				append(options.title != null && options.title.equals("title") ? "`title`" : "'' as `title`").
 				append(", sum(`viewers`) as viewers, sum(`duration`) as duration, toTS from ").
 				append(getTable(from, to)).
 				append(" where type = ? and toTS > ? and toTS <= ?  ").
-				append(options.getValue0() != null && options.getValue0().equals("title") ? 
+				append(options.title != null && options.title.equals("title") ? 
 						" group by type, name, title order by " : " group by type, name order by ").
-				append(options.getValue1()).append(" ").
-				append(options.getValue2()).
+				append(options.orderBy).append(" ").
+				append(options.order).
 				append(" limit 0, ?");
 		List<Map<String, Object>> resultList = getJdbcTemplate().queryForList(
 				q.toString(),
-				type, from, to, options.getValue3());
+				type, from, to, options.size);
 		for (Map<String, Object> row : resultList) {
 			ListResult<Object> rec = new ListResult<Object>();
 			for (String col : StatsDataProcessor.topViewColumns) {
 				if(col.equals("time")){
-					rec.addElement(getFormattedTime(options.getValue4(), (Long)row.get("toTS")));
+					rec.addElement(getFormattedTime(options.timeunit, (Long)row.get("toTS")));
 				}else{
 					rec.addElement(row.get(col));
 				}
@@ -271,15 +271,16 @@ public class StatsDataProvider {
 	private final static DateTimeFormatter fmtDay = ISODateTimeFormat.date();
 	private final static DateTimeFormatter fmtMonth = ISODateTimeFormat.yearMonth();
 	private final static DateTimeFormatter fmtWeek = ISODateTimeFormat.weekyearWeek();
-	private String getFormattedTime(String timeunit, long ts){
-		DateTime dt = new DateTime(ts);			
-		if(timeunit.equalsIgnoreCase("daily")){
+	private String getFormattedTime(TimeUnit timeunit, long ts){
+		DateTime dt = new DateTime(ts);	
+		switch(timeunit){
+		case daily:
 			return fmtDay.print(dt);
-		}else if(timeunit.equalsIgnoreCase("weekly")){
+		case weekly:
 			return fmtWeek.print(dt);
-		}else if(timeunit.equalsIgnoreCase("monthly")){
+		case monthly:
 			return fmtMonth.print(dt);
-		}else{
+		default:
 			return fmtHour.print(dt);
 		}
 	}
@@ -335,25 +336,56 @@ public class StatsDataProvider {
 		}
 		return "stats_view_daily";
 	}	
-	
-	private Quintet<String, String, String, Integer, String> extractOptions(String options){
-		Quintet<String, String, String, Integer, String> opts = new Quintet<String, String, String, Integer, String>(null, "viewers", "desc", 10, "hourly");
-		if(options != null){
-			String[] os = options.split(",");
-			for (String st : os){
-				if(st.equalsIgnoreCase("title")){
-					opts = opts.setAt0("title");
-				}else if(st.equalsIgnoreCase("duration")){
-					opts = opts.setAt1("duration");
-				}else if(st.equalsIgnoreCase("low")){
-					opts = opts.setAt2("asc");
-				}else if(st.matches("[1-9][0-9]*")){
-					opts = opts.setAt3(Integer.valueOf(st));
-				}else if(st.equalsIgnoreCase("weekly") || st.equalsIgnoreCase("monthly") || st.equalsIgnoreCase("daily")){
-					opts = opts.setAt4(st);
+		
+	static class Options {
+		String title;
+		String orderBy = "viewers";
+		String order = "desc";
+		TimeUnit timeunit = TimeUnit.hourly;
+		int size = 10;
+		Options(String options){
+			if(options != null){
+				String[] os = options.split(",");
+				for (String st : os){
+					if(st.equalsIgnoreCase("title")){
+						title = "title";
+					}else if(st.equalsIgnoreCase("duration")){
+						orderBy = "duration";
+					}else if(st.equalsIgnoreCase("low")){
+						order = "asc";
+					}else if(st.matches("[1-9][0-9]*")){
+						size = Integer.valueOf(st);
+					}else if(st.equalsIgnoreCase(TimeUnit.weekly.name()) || 
+							st.equalsIgnoreCase(TimeUnit.monthly.name()) || 
+							st.equalsIgnoreCase(TimeUnit.daily.name())){
+						timeunit = TimeUnit.valueOf(st);
+					}
 				}
 			}
 		}
-		return opts;
-	}	
+		public Options setTitle(String title) {
+			this.title = title;
+			return this;
+		}
+		public Options setOrderBy(String orderBy) {
+			this.orderBy = orderBy;
+			return this;
+		}
+		public Options setOrder(String order) {
+			this.order = order;
+			return this;
+		}
+		public Options setTimeunit(TimeUnit timeunit) {
+			this.timeunit = timeunit;
+			return this;
+		}
+		public Options setSize(int size) {
+			this.size = size;
+			return this;
+		}
+	}
+	
+	enum TimeUnit{
+		hourly, daily, weekly, monthly;
+	}
 }
